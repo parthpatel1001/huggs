d12337e088b11a5c1d43698c8e89514bf45aafe8
parthpatel1001 <parthpatel1001@gmail.com>
2015-01-21 00:46:50 -0800

    allow passing an id to lookup a flight and change the way we cache

--- modules/flightsAPI.js
+++ modules/flightsAPI.js
@@ -19,7 +19,49 @@ module.exports = {
 		var getFlightKeyString = function(airline,flight_num) {
 			return airline + REDIS_KEY_SEPERATOR + flight_num;
 		};
+		var getFormattedFlightInfoResult = function(rawApiResponseData) {
+			var depart_time,arrive_time,from_airport,dest_airport,
+				flight_info = rawApiResponseData;
 
+			if(flight_info.actualdeparturetime) {
+				depart_time = flight_info.actualdeparturetime;
+			} else {
+				depart_time = flight_info.filed_departuretime;
+			}
+
+			if(flight_info.actualarrivaltime) {
+				arrival_time = flight_info.actualarrivaltime;
+			} else {
+				arrival_time = flight_info.estimatedarrivaltime;
+			}
+
+			var arrival_date_compare = new Date(arrival_time*1000).toDateString();
+			var today_date_compare = new Date().toDateString();
+			
+			from_airport = flight_info.origin;
+			dest_airport = flight_info.destination;
+			from_airport = from_airport.substr(from_airport.length - 3);
+			dest_airport = dest_airport.substr(dest_airport.length - 3);
+			faFlightId   = flight_info.faFlightID;
+			
+			return {
+		  			from_airport      : from_airport,
+		  			dest_airport      : dest_airport,
+		  			depart_time       : depart_time,
+		  			arrive_time       : arrival_time,
+		  			faId              : faFlightId
+	  		};
+		};
+		var getFlightInfoFromFlightAwareAPI = function(airline,flight_num,callback) {
+			restclient.get(ENVIRONMENT.fxml_url+'FlightInfoEx',{
+				username: ENVIRONMENT.username,
+				password: ENVIRONMENT.apiKey,
+				query: {
+					ident:airline+flight_num,
+					howMany: 10
+				}
+			}).on('success',callback);
+		}
 		this.setFlightRequestToCache = function(airline,flight_num,data) {
 			var flightKeyString = getFlightKeyString(airline,flight_num);
 			this.redisClient.set(flightKeyString,JSON.stringify(data));
@@ -28,76 +70,53 @@ module.exports = {
 
 		//gets the flight data from cache if there
 		//if not, reaches out to the api and stores it in cache
-		this.getFlightData = function(airline,flight_num, callback) {
+		this.getFlightData = function(airline,flight_num, id, callback) {
 			this.redisClient.get(getFlightKeyString(airline,flight_num),function(err,reply){
 				if(reply) {
-					callback(JSON.parse(reply));
+					var flightListFromCache = JSON.parse(reply);
+					if(flightListFromCache.error) {
+						return callback({error:true});
+					}
+					
+					if(id === undefined || id === null) {
+						return callback(flightListFromCache);
+					}
+
+					for(var i in flightListFromCache) {
+						if(flightListFromCache[i].faId == id) {
+							return callback(flightListFromCache[i]);
+						}
+					}
+					return callback({error:true});
 				} else {
 					if(err) {
 						console.log('error getting flight data :'+err);
-						callback('error getting flight data');
+						return callback({error:true});
 					} else {
-						restclient.get(ENVIRONMENT.fxml_url+'FlightInfoEx',{
-							username: ENVIRONMENT.username,
-							password: ENVIRONMENT.apiKey,
-							query: {
-								ident:airline+flight_num
-							}
-						}).on('success',function(result,response){
-							if(!response.error && result.FlightInfoExResult && result.FlightInfoExResult.flights && result.FlightInfoExResult.flights[0]) {
-								var depart_time,arrive_time,from_airport,dest_airport;
-								//go through results and find the flight that arrives today
-								//change this later to use user selected arrival date
-								for(var i=0; i < result.FlightInfoExResult.flights.length; i++) {
-									flight_info = result.FlightInfoExResult.flights[i];
-
-									if(flight_info.actualdeparturetime) {
-										depart_time = flight_info.actualdeparturetime;
-									} else {
-										depart_time = flight_info.filed_departuretime;
-									}
-
-									if(flight_info.actualarrivaltime) {
-										arrival_time = flight_info.actualarrivaltime;
-									} else {
-										arrival_time = flight_info.estimatedarrivaltime;
-									}
-
-									var arrival_date_compare = new Date(arrival_time*1000).toDateString();
-									var today_date_compare = new Date().toDateString();
-									
-									if(arrival_date_compare == today_date_compare) {
-										break;
-									}
+						if(id === undefined){ id = null;}
+						getFlightInfoFromFlightAwareAPI(airline,flight_num,function(result,response){
+							if(!response.error && result.FlightInfoExResult && 
+								result.FlightInfoExResult.flights && result.FlightInfoExResult.flights[0]) {
+								var formmatedFlightData = [],
+									rawFlightData = result.FlightInfoExResult.flights;
+								for(var i in rawFlightData) {
+									formmatedFlightData.push(getFormattedFlightInfoResult(rawFlightData[i]));
 								}
-
-								from_airport = flight_info.origin;
-								dest_airport = flight_info.destination;
-								from_airport = from_airport.substr(from_airport.length - 3);
-								dest_airport = dest_airport.substr(dest_airport.length - 3);
-								var responseToCache = {
-							  			from_airport      : from_airport,
-							  			dest_airport      : dest_airport,
-							  			depart_time       : depart_time,
-							  			arrive_time       : arrival_time
-						  		};
-
-								that.setFlightRequestToCache(
-									airline,
-									flight_num,
-									responseToCache
-								);
-								that.getFlightData(airline,flight_num,callback);
+								
+								that.setFlightRequestToCache(airline,flight_num,formmatedFlightData);
+								
+								that.getFlightData(airline,flight_num,id,callback);
 
 							} else {
-								callback('error getting flight data');
+								that.setFlightRequestToCache(airline,flight_num,{error:true});
+								return callback({error:true});
 							}
 
 						});
 					}
 				}
 			});
-		}
+		};
 
 		return this;
 	}
